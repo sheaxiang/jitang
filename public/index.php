c7cd4e94caeecc31838cab8580fa5c8cbdafd41c
sheaxiang <642290531@qq.com>
2018-07-12 10:51:25 +0800

    update

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
-
+var_dump(1);
+exit;
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
